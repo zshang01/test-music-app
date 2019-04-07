38d9b30f787df6046d2857ff409a04fb70093917
Zhou Shang <alex.zhou.shang>
2019-04-06 20:56:15 -0700

    100

---
+++ imports/api/Users.js
@@ -0,0 +1,70 @@
+
+
+
+import { Meteor } from "meteor/meteor";
+import { Mongo } from "meteor/mongo";
+import { check } from "meteor/check";
+
+export const Users = new Mongo.Collection("Users");
+
+if (Meteor.isServer) {
+	Meteor.publish('Users', () => Users.find({}))
+}
+
+Meteor.methods({
+	'user.signup': function(param){
+		// check(username, String)
+		// check(email, String)
+		// check(password, String)
+		Users.insert({
+			createAt: Date.now(),
+			name: param.username,
+			email: param.email,
+			password: param.password,
+			history:[]
+		});
+		const user = Users.find({}).fetch();
+		
+		
+		return user[0];
+	},
+	'user.comment': function(param){
+		console.log("server side")
+		const email = param.email
+		
+		const name = param.name
+		
+		const user = Users.find({ email: email }).fetch();
+		// console.log(user);
+		// const u = user.filter( us => {return us.email === email})
+		// console.log(typeof user[1].email)
+		// console.log(typeof email)
+		// console.log(u)
+		
+		const pre = user[0].history;
+		
+		pre.push(name);
+
+		
+		Users.update(
+			{ _id: user[0]._id },
+			{
+				$push: {
+					 history: name
+				}
+			}	
+		)
+		
+		return user[0];
+	},
+	'user.lastFive': function(email){
+		const user = Users.find({ email: email }).fetch();
+		const res = []
+		for(var i = 0; i < user[0].history.length; i += 1){
+			res.push(user[0].history[i])
+			if(res.length == 5) break;
+		}
+		return res
+	}
+})
+
