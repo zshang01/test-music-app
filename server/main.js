df7916aa39d0dbbe9406c926f76761bfb31edc3f
Zhou Shang <alex.zhou.shang>
2019-04-11 19:46:48 -0700

    103

--- server/main.js
+++ server/main.js
@@ -3,6 +3,37 @@ import "../imports/api/Song.js";
 import "../imports/api/Methods.js";
 import "../imports/api/Users.js";
 
+import { DDPRateLimiter } from "meteor/ddp-rate-limiter";
+import { WebApp } from "meteor/webapp";
+
+const LISTS_METHODS = [
+  "displayGenres",
+  "search",
+  "searchSong"
+];
+
+// Only allow 5 list operations per connection per second
+
+if (Meteor.isServer) {
+  DDPRateLimiter.addRule(
+    {
+      name(name) {
+        return LISTS_METHODS.includes(name);
+      },
+
+      // Rate limit per connection ID
+      connectionId() {
+        return true;
+      }
+    },
+    5,
+    1000
+  );
+}
+
+
+WebApp.addHtmlAttributeHook(() => ({ lang: "en" }));
+
 Meteor.startup(() => {
   // code to run on server at startup  
 
